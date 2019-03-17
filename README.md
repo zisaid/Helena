4b68ad1b0b5b79d8c0e7b50f8be5e6479abc7485
f <f@f.f>
2019-03-17 18:36:00 +0800

    1.0.5 增加了执行前的插件，可以在response前加自定义header

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Helena
+# 日志
+
+- 1.0.5 增加了执行前的插件，可以在response前加自定义header
--- test/server.test.js
+++ test/server.test.js
@@ -6,4 +6,13 @@ let testHiRouter = require('./router.hi.test');
 server.use('/hello', testHelloRouter);
 server.use('/hi', testHiRouter);
 
+let allowCrossDomain = function (req, res) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+};
+
+server.use(allowCrossDomain);
+
 server.start(3109);
