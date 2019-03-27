6e5c9618e23044a1441eadd378f7374507cf4e6c
F <F@F.F>
2019-03-27 17:42:24 +0800

    - 1.0.25code

--- biz/code.js
+++ biz/code.js
@@ -43,7 +43,7 @@ code.onReady = function () {
                         mkArr(son, arr);
                     }
                 };
-
+                codes = {};
                 mongodb.read(db, table)
                     .then(res => {
                         for (let re of res) {
@@ -70,19 +70,19 @@ code.getCode = function (type) {
 /**
  * 代码转标签
  * @param type
- * @param code
+ * @param nodeCode
  */
-code.code2labels = function (type, code) {
-    let result = codes[type + 'Arr'][code].label;
-    while (codes[type + 'Arr'][code].fid > 0) {
-        code = codes[type + 'Arr'][code].fid;
-        result = codes[type + 'Arr'][code].label + '-' + result;
+code.code2labels = function (type, nodeCode) {
+    let result = codes[type + 'Arr'][nodeCode].label;
+    while (codes[type + 'Arr'][nodeCode].fid > 0) {
+        nodeCode = codes[type + 'Arr'][nodeCode].fid;
+        result = codes[type + 'Arr'][nodeCode].label + '-' + result;
     }
     return result;
 };
 
-code.code2label = function (type, code) {
-    return codes[type + 'Arr'][code].label;
+code.code2label = function (type, nodeCode) {
+    return codes[type + 'Arr'][nodeCode].label;
 };
 
 /**
@@ -91,14 +91,14 @@ code.code2label = function (type, code) {
  * @param label
  */
 code.label2code = function (type, label) {
-    let code;
+    let nodeCode;
     for (let node of codes[type + 'Arr']) {
         if (node.label === label) {
-            code = node.id;
+            nodeCode = node.id;
             break;
         }
     }
-    return code;
+    return nodeCode;
 };
 
 /**
@@ -127,9 +127,9 @@ code.labels2code = function (type, labelList) {
     return result;
 };
 
-code.code2codes = function (type, code) {
-    let item = codes[type + 'Arr'][code];
-    let single = [code];
+code.code2codes = function (type, nodeCode) {
+    let item = codes[type + 'Arr'][nodeCode];
+    let single = [nodeCode];
     let loop = true;
     let temp = item;
     while (loop) {
@@ -169,38 +169,196 @@ code.sort = function (type, c1, c2) {
 };
 
 code.save2db = function (type) {
-    mongodb.update(db, table, {label: type}, codes[type]);
-};
-
-code.moveSubtree = function (type, nodeCode, toCode, way) {
     let result = false;
-    let codesType = codes[type + 'Arr'];
-    //要做为某个节点的子节点移入，这个节点的子节点必须为空。如果不空，需要用节点前或后的方式
-    if (way > 0 || codesType[toCode].children.length < 1) {
-        //从原父节点断开
-        let fatherCode = codesType[nodeCode].fid;
-        let site = -1;
-        for (let i = 0; i < codesType[fatherCode].children.length; i++) {
-            if (codesType[fatherCode].children[i].id === nodeCode) {
-                site = i;
-                break;
-            }
-        }
-        if (site > -1) codesType[fatherCode].children.splice(site, 1);
-        //连接新的父结点
-        if(way === 0) {
-            codesType[nodeCode].fid = toCode;
-            codesType[toCode].children.push(codesType[nodeCode]);
-        } else {
-            codesType[nodeCode].fid = codesType[toCode].fid;
-            if(way > 0) site++;
-            codesType[fatherCode].children.splice(site, 0, codesType[nodeCode]);
-        }
+    if (codes[type]) {
+        mongodb.update(db, table, {label: type}, codes[type]);
         result = true;
     }
     return result;
 };
 
+code.create = function (type) {
+    let result = false;
+    if (!codes[type]) {
+        codes[type] = {
+            'label': type,
+            'id': 0,
+            'fid': -1,
+            'idcount': 0,
+            'disabled': false,
+            'children': []
+        };
+        codes[type + 'Arr'][0] = codes[type];
+        code.save2db(type);
+        result = true;
+    }
+    if (result) result = codes[type];
+    return result;
+};
+
+code.modifyLabel = function (type, nodeCode, label) {
+    let result = false;
+    if (nodeCode > 0 && codes[type + 'Arr'] && codes[type + 'Arr'][nodeCode]) {
+        codes[type + 'Arr'][nodeCode].label = label;
+        nodeCode.save2db(type);
+        result = true;
+    }
+    if (result) result = codes[type];
+    return result;
+};
+
+code.moveSubtree = function (type, nodeCode, toNodeCode, way) {
+    let result = false;
+    let codesType = codes[type + 'Arr'];
+    //要做为某个节点的子节点移入，这个节点的子节点必须为空。如果不空，需要用节点前或后的方式
+    if (nodeCode > 0 && toNodeCode > 0
+        && codesType && codesType[nodeCode] && codesType[toNodeCode]
+        && (way === code.SON || way === code.AFTER || way === code.BEFORE)
+        && (way !== code.SON || codesType[toNodeCode].children.length < 1)) {
+        //从原父节点断开
+        let fatherCode = codesType[nodeCode].fid;
+        if (fatherCode > -1) {
+            let site = -1;
+            for (let i = 0; i < codesType[fatherCode].children.length; i++) {
+                if (codesType[fatherCode].children[i].id === nodeCode) {
+                    site = i;
+                    break;
+                }
+            }
+            if (site > -1) codesType[fatherCode].children.splice(site, 1);
+            //连接新的父结点
+            if (way === code.SON) {
+                codesType[nodeCode].fid = toNodeCode;
+                codesType[toNodeCode].children.push(codesType[nodeCode]);
+            } else {
+                codesType[nodeCode].fid = codesType[toNodeCode].fid;
+                if (way === code.AFTER) site++;
+                codesType[fatherCode].children.splice(site, 0, codesType[nodeCode]);
+            }
+            result = true;
+            code.save2db(type);
+        }
+    }
+    if (result) result = codes[type];
+    return result;
+};
+
+code.copySubtree = function (type, nodeCode, toNodeCode, way) {
+    let result = false;
+    let codesType = codes[type + 'Arr'];
+    //要做为某个节点的子节点移入，这个节点的子节点必须为空。如果不空，需要用节点前或后的方式
+    if (nodeCode > 0 && toNodeCode > 0
+        && codesType && codesType[nodeCode] && codesType[toNodeCode]
+        && (way === code.SON || way === code.AFTER || way === code.BEFORE)
+        && (way !== code.SON || codesType[toNodeCode].children.length < 1)) {
+        let copy = function (from, fid) {
+            codesType[0].idcount++;
+            let to = {};
+            to.id = codesType[0].idcount;
+            to.fid = fid;
+            to.disabled = false;
+            to.label = from.label;
+            to.children = [];
+            from.children.forEach(son => {
+                let sonNode = copy(son, to.id);
+                to.children.push(sonNode);
+            });
+            codesType[to.id] = to;
+            return to;
+        };
+        let newNode = copy(codesType[nodeCode], 0);
+        newNode.label = '<副本>' + newNode.label;
+        if (way === code.SON) {
+            newNode.fid = codesType[toNodeCode].id;
+            codesType[toNodeCode].children.push(newNode);
+        } else {
+            newNode.fid = codesType[toNodeCode].fid;
+            let children = codesType[toNodeCode.fid].children;
+            let site = -1;
+            for (let i = 0; i < children.length; i++) {
+                if (children[i].id === toNodeCode) {
+                    site = i;
+                    break;
+                }
+            }
+            if (site > -1) {
+                if (way === code.AFTER) site++;
+                children.splice(site, 0, newNode);
+            }
+            result = true;
+            code.save2db(type);
+        }
+        if (result) result = codes[type];
+        return result;
+    }
+};
+
+code.changeSubtreeDisabledValue = function (type, nodeCode, disabled) {
+    let result = false;
+    let codesType = codes[type + 'Arr'];
+    let f = function (type, nodeCode, disabled) {
+        codesType[nodeCode].disabled = disabled;
+        codesType[nodeCode].children.forEach(sonNode => {
+            f(type, sonNode, disabled);
+        });
+    };
+    if (codesType && codesType[nodeCode]) {
+        f(type, nodeCode, disabled);
+        code.save2db(type);
+        result = true;
+    }
+    if (result) result = codes[type];
+    return result;
+};
+
+code.addSubtree = function (type, labelList, toNodeCode, way) {
+    let result = false;
+    if (codes[type + 'Arr'] && codes[type + 'Arr'][toNodeCode]
+        && (way === code.SON || way === code.AFTER || way === code.BEFORE)
+        && (way !== code.SON || codes[type + 'Arr'][toNodeCode].children.length < 1)) {
+        let node = codes[type + 'Arr'][toNodeCode];
+        let newNodes = [];
+        labelList.forEach(label => {
+            newNodes.push({label: label, id: 0, fid: -1, disabled: false, children: []});
+        });
+        if (way === code.SON) {
+            newNodes.forEach(newNode => {
+                codes[type + 'Arr'][0].idcount++;
+                newNode.id = codes[type + 'Arr'][0].idcount;
+                newNode.fid = node.id;
+                node.children.push(newNode);
+                codes[type + 'Arr'][newNode.id] = newNode;
+            });
+            code.save2db(type);
+            result = true;
+        } else {
+            if (node.fid > -1) {
+                let children = codes[type + 'Arr'][node.fid].children;
+                let site = -1;
+                for (let i = 0; i < children.length; i++) {
+                    if (children[i].id === toNodeCode) {
+                        site = i;
+                        break;
+                    }
+                }
+                if (site > -1) {
+                    if (way === code.AFTER) site++;
+                    newNodes.forEach(newNode => {
+                        codes[type + 'Arr'][0].idcount++;
+                        newNode.id = codes[type + 'Arr'][0].idcount;
+                        newNode.fid = node.fid;
+                        codes[type + 'Arr'][newNode.id] = newNode;
+                    });
+                    children.splice(site, 0, ...newNodes);
+                    code.save2db(type);
+                    result = true;
+                }
+            }
+        }
+    }
+    if (result) result = codes[type];
+    return result;
+};
 
 module.exports = code;
 
